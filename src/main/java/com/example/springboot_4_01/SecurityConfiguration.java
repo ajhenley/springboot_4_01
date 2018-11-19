7ab21096824a2341f672e7c3168e247f7e97d3d8
AJ Henley <ajhenley@gmail.com>
2018-11-19 05:34:08 -0500

    user registration

--- src/main/java/com/example/springboot_4_01/SecurityConfiguration.java
+++ src/main/java/com/example/springboot_4_01/SecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
             .authorizeRequests()
-            .antMatchers("/","/h2-console/**").permitAll()
+            .antMatchers("/","/h2-console/**","/register").permitAll()
             .antMatchers("/secure").access("hasAnyAuthority('USER','ADMIN')")
             .antMatchers("/admin").access("hasAuthority('ADMIN')")
             .anyRequest().authenticated()
--- src/main/java/com/example/springboot_4_01/UserRepository.java
+++ src/main/java/com/example/springboot_4_01/UserRepository.java
@@ -6,4 +6,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
   User findByUsername(String username);
   User findByEmail(String email);
   User findByEmailAndPassword(String email, String password);
+  Long countByEmail(String email);
+  Long countByUsername(String username);
 }
