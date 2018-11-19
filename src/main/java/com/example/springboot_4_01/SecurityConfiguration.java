b6121fb3e9431ace83ed45f11258978debfebf32
AJ Henley <ajhenley@gmail.com>
2018-11-19 02:20:36 -0500

    added role permissions

--- src/main/java/com/example/springboot_4_01/SecurityConfiguration.java
+++ src/main/java/com/example/springboot_4_01/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -21,15 +22,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
             .authorizeRequests()
+            .antMatchers("/")
+            .access("hasAnyAuthority('USER','ADMIN')")
+            .antMatchers("/admin").access("hasAuthority('ADMIN')")
             .anyRequest().authenticated()
             .and()
-            .formLogin().loginPage("/login").permitAll();
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/login").permitAll();
   }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth)throws Exception{
     auth.inMemoryAuthentication().withUser("user")
             .password(passwordEncoder().encode("password"))
-            .authorities("USER");
+            .authorities("USER")
+            .and()
+            .withUser("dave")
+            .password(passwordEncoder().encode("begreat"))
+            .authorities("ADMIN");
   }
 }
